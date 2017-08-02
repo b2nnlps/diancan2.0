0eb4994af8756d979e35c537ad35793caf4f849f
Neil <674753084@qq.com>
2017-08-02 19:01:22 +0800

    添加MD5验证

--- app/src/main/java/cn/n39/ms/diancan/ToastUtil.java
+++ app/src/main/java/cn/n39/ms/diancan/ToastUtil.java
@@ -3,6 +3,10 @@ package cn.n39.ms.diancan;
 import android.content.Context;
 import android.widget.Toast;
 
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 /**
  * Created by qs on 2016/10/17.
  */
@@ -13,5 +17,28 @@ public class ToastUtil {
     public static void showToast(Context context, String str) {
 
         Toast.makeText(context, str, Toast.LENGTH_SHORT).show();
+    }
+
+    public static String stringToMD5(String string) {
+        byte[] hash;
+
+        try {
+            hash = MessageDigest.getInstance("MD5").digest(string.getBytes("UTF-8"));
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            return null;
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+        StringBuilder hex = new StringBuilder(hash.length * 2);
+        for (byte b : hash) {
+            if ((b & 0xFF) < 0x10)
+                hex.append("0");
+            hex.append(Integer.toHexString(b & 0xFF));
+        }
+
+        return hex.toString();
     }
 }
